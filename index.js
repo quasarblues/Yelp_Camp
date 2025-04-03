f70ebce432fc5860fa0bf0d3f420d70758247564
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-04-03 11:26:21 +0900

    Added login page and login routes

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 
 // Routes
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const campgroundRoutes = require('./routes/campgrounds');
+const userRoutes = require('./routes/users');
+const reviewRoutes = require('./routes/reviews');
 
 // Custom Utilities
 const ExpressError = require('./utils/ExpressError');
@@ -27,17 +28,17 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     })
     .catch((err) => {
         console.log(`Error of ${err}`)
-    })
+    });
 
 // Core Express Setup
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware Setup
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
     secret: 'thisisasecret',
@@ -49,17 +50,17 @@ const sessionConfig = {
         // maxAge is 1 week
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
-}
-app.use(session(sessionConfig))
-app.use(flash())
+};
+app.use(session(sessionConfig));
+app.use(flash());
 
 // Set up passport
-app.use(passport.initialize())
-app.use(passport.session())
-passport.use(new LocalStrategy(User.authenticate()))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
@@ -68,33 +69,25 @@ app.use((req, res, next) => {
     next();
 })
 
-app.get('/fakeUser', async (req, res) => {
-    const user = new User({
-        email: 'alamaba@gmail.com',
-        username: 'alabama man'
-    })
-    const newUser = await User.register(user, 'superman');
-    res.send(newUser)
-})
-
 // Route Setup (Express Router)
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use(userRoutes);
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
+    next(new ExpressError('Page Not Found', 404));
 })
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh no, there was a problem'
     res.status(statusCode).render('error', { err })
-})
+});
 
 app.listen(3000, () => {
-    console.log('Serving on port 3000')
+    console.log('Serving on port 3000');
 })
