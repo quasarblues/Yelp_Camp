d87c3763cbadfede60c5b33089c991dc2bd6bccb
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-04-08 21:45:37 +0900

    Return user to previous page after logging in

--- middleware.js
+++ middleware.js
@@ -1,11 +1,18 @@
 const isLoggedIn = (req, res, next) => {
-    console.log("REQ.USER...", req.user);
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'Please sign in.');
         return res.redirect('/login');
     }
     next();
 }
 
-module.exports = isLoggedIn;
+const storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
+}
+
+module.exports = { isLoggedIn, storeReturnTo };
 
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const isLoggedIn = require('../middleware');
+const { isLoggedIn } = require('../middleware');
 
 const Campground = require('../models/campground');
 const { campgroundSchema } = require('../schemas');
