c9771c124ec2dabcf7e651827d7ba73429c6047c
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-04-14 10:32:20 +0900

    Deleted an empty line

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const { validateReview } = require('../middleware');
-
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
