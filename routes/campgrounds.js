c7127305ed72130013eeb5c82f8c40fa4c36308d
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-04-14 09:15:27 +0900

    Only campground author can make a PUT request.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -57,6 +57,11 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
 
 router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that.');
+        return res.redirect(`/campgrounds/${id}`);
+    }
     await Campground.findByIdAndUpdate(id, req.body.campground, { new: true });
     req.flash('success', 'Campground successfully updated.');
     res.redirect(`/campgrounds/${id}`);
