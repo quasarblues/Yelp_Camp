512a1f6f038dc0b6953354f63e7c9f651ea6f5d8
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-05-18 22:42:55 +0900

    Created ImageSchema. Added thumbnail virtual to ImageSchema.

--- models/campground.js
+++ models/campground.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/dlvbewmvi/image/upload/v1745974594/YelpCamp/objcfcc1n4fe2wtqzspb.jpg
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_300');
+})
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
