4927472ed76e9e55d261d2d8e0caf11dc75b5dba
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-05-23 21:57:15 +0900

    Added mapbox pop-ups

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -8,4 +8,10 @@ const map = new mapboxgl.Map({
 
 new mapboxgl.Marker()
     .setLngLat([-74.5, 40])
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+    )
     .addTo(map)
