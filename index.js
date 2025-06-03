d838bdeaef2db14a71fa0a337357d38d16ec97eb
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-06-03 18:46:30 +0900

    Sanitize express mongo and html inputs

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const User = require('./models/user');
 // Middleware
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const mongoSanitize = require('express-mongo-sanitize');
 
 // Routes
 const campgroundRoutes = require('./routes/campgrounds');
@@ -44,6 +45,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: 'thisisasecret',
@@ -68,6 +70,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    console.log(req.query);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
