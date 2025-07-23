9d164ba9d48975417ab2765568faa1b2560c0f58
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-07-23 10:39:47 +0900

    WIP edit reviews

--- public/javascripts/editReviews.js
+++ public/javascripts/editReviews.js
@@ -5,7 +5,10 @@
 // I don't need a route for the review update, since it'll be done on the show page.
 
 const editReviewBtn = document.querySelector('#editReview');
+let prevRating = review.rating;
 
 editReviewBtn.addEventListener('click', () => {
-    alert('You clicked me');
+    console.log('you clicked');
 })
+
+console.log(prevRating);
