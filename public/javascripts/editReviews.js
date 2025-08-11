1b00765e8e7c73b15d6e38744091cf5861114bc5
Todd Basham <20284124+quasarblues@users.noreply.github.com>
2025-08-11 21:17:10 +0900

    WIP edit comments

--- public/javascripts/editReviews.js
+++ public/javascripts/editReviews.js
@@ -1,13 +1,9 @@
-// add event listener to the edit button.
-// replace the element with a form + update button.
-// clicking update submits the form
-// make a route to handle the review update.
-// I don't need a route for the review update, since it'll be done on the show page.
+const editReviewBtns = document.querySelectorAll('.editReview');
+const editReviewForms = document.querySelectorAll('.editReviewForm');
 
-const editReviewBtn = document.querySelector('#editReview');
-const editReviewForm = document.querySelector('#editReviewForm');
-
-editReviewBtn.addEventListener('click', () => {
-    editReviewForm.classList.remove('d-none');
+editReviewBtns.forEach(editBtn => {
+  editBtn.addEventListener('click', () => {
+    console.log('chocolate time');
+  });
 })
 
